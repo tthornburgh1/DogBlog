caa176636e1f1b4f90271ba015168f3e5d154ed0
Tristan Thornburgh <tristan.thornburgh@gmail.com>
2018-02-08 17:01:57 -0800

    added buttons and jQuery functionality to dog page

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("button#black").click(function() {
+    $("body").removeClass();
+    $("body").addClass("black-background");
+  });
+
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+  });
+});
